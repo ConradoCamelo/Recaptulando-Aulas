9722811b1158d21a4c76e139c5ccd2ff75f8dfde
Conrado Camelo <Conrado Camelo@DESKTOP-OKUO54V>
2022-12-26 00:24:02 -0300

    CM02 - Programa Soma e Contador

---
+++ FormacaoJavaWeb/src/Recaptulando/Soma.java
@@ -0,0 +1,26 @@
+package Recaptulando;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		
+		int a, b, result;
+		
+		a = 5;
+		b = 7;
+				
+		result = a+b;
+		
+		System.out.println("O resultado de " + a + " + " + b + " é igual a: " +  result);
+		
+		
+		System.out.println("Conte de 01 até 100");
+		
+		for (int i =1; i<=1000; i++) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
